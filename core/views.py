f9005d4a24c8f080f6549adb86e6979146fa94bd
VishDroid-dev <vk2345732@gmail.com>
2021-06-19 22:01:20 +0530

    api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Question, Answer
+from .serializers import QuestionSerializer, AnswerSerializer
+
+
+class QuestionView(APIView):
+	def get(self, request, format=None):
+		objs = Answer.objects.all()
+		serializer = QuestionSerializer(objs, many=True)
+		return Response(
+			serializer.data
+			)
